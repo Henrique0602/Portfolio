9c17d09cf6d1aa1ac93948cc1405551f57b269ac
Henrique0602 <henriquef.brigo@gmail.com>
2024-03-08 21:15:10 -0300

    Portifolio Henrique

---
+++ script.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset : true});
+
+
+sr.reveal('.home',{duration: 2000});
+
+sr.reveal('.sobre',{duration: 3000});
+
+sr.reveal('.projetos',{duration: 4000});
+
+sr.reveal('.contato',{duration: 5000});
